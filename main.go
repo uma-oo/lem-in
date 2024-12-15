e18882a09e544fec395f403ba18edbb78004864f
ooumayma <oumayma.elfahsi99@gmail.com>
2024-12-15 17:11:00 +0100

    add files now

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"lemin/helpers"
+)
+
+func main() {
+	args := os.Args[1:]
+	switch len(args) {
+	case 0:
+		fmt.Println("USAGE: go run . file.txt")
+	case 1:
+		colony, err :=helpers.Parse(args[0])
+		if err!=nil {
+			fmt.Println(err)
+			return 
+		}
+		fmt.Println(colony.String())
+
+	default:
+		fmt.Println("USAGE: go run . file.txt")
+	}
+}
